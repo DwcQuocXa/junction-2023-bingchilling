160cf5bdda6c5598f88320654630b0935d8d92ea
Duc Ngo <duc.ngo@weare.fi>
2023-11-10 19:25:46 +0200

    new pages and stuffs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,23 @@
 import { View } from '@bacons/react-views';
 import { Stack } from 'expo-router';
 import React from 'react';
+import { Text } from 'react-native';
 
+import { ScreenHeaderButton } from '../components';
 import Footer from '../components/common/Footer';
+import { COLORS } from '../constants';
 
 type Props = object;
 
 const Layout = (props: Props) => {
     return (
         <Stack>
-            <Stack.Screen name="(tabs)" />
+            <Stack.Screen
+                name="(tabs)"
+                options={{
+                    headerShown: false, //header shown or not
+                }}
+            />
         </Stack>
     );
 };
