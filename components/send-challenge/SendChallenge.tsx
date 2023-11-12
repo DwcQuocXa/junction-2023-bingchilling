50f0bb87185bea3cde163857b5e9761fda98a591
Duy Anh Vu <duy.a.vu@tuni.fi>
2023-11-12 02:30:49 +0200

    Add group to send challenge

--- components/send-challenge/SendChallenge.tsx
+++ components/send-challenge/SendChallenge.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import { SearchBar } from 'react-native-elements';
-
+import { AntDesign } from '@expo/vector-icons';
 import ImageData from '../../assets/images/generations.json';
 import UserData from '../../assets/users.json';
 import AvatarCard from '../common/AvatarCard';
+import { TouchableOpacity } from 'nativewind/dist/preflight';
+import { router } from 'expo-router';
 
 const SendChallenge = () => {
     const [query, setQuery] = useState('');
@@ -35,33 +37,88 @@ const SendChallenge = () => {
                 autoCorrect={false}
                 containerStyle={styles.searchBar}
             />
-            {filteredData.map((user, idx) => (
+            <Text style={styles.sectionText}>Friends</Text>
+            <View style={styles.cardContainer}>
+                {filteredData.map((user, idx) => (
+                    <AvatarCard
+                        key={user.id}
+                        id={user.id}
+                        avatarUrl={ImageData.generations[idx].generated_images[0].url}
+                        name={user.name}
+                    />
+                ))}
+            </View>
+            <Text style={styles.sectionText}>Groups</Text>
+            <View style={styles.cardContainer2}>
                 <AvatarCard
-                    key={user.id}
-                    id={user.id}
-                    avatarUrl={ImageData.generations[idx].generated_images[0].url}
-                    name={user.name}
+                    key={111}
+                    id="1"
+                    avatarUrl={ImageData.generations[4].generated_images[0].url}
+                    name="Bing chiling"
                 />
-            ))}
+                <AvatarCard
+                    key={122}
+                    id="2"
+                    avatarUrl={ImageData.generations[4].generated_images[3].url}
+                    name="Avengers"
+                />
+                <TouchableOpacity
+                    style={styles.addGroupButton}
+                    onPress={() => router.push('/create-group')}
+                >
+                    <AntDesign name="addusergroup" size={56} color="black" />
+                </TouchableOpacity>
+            </View>
         </ScrollView>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
-        alignItems: 'center',
         justifyContent: 'center',
         padding: 10,
         // If you want a grid of 2x4
-        flexDirection: 'row',
+        flexDirection: 'column',
         flexWrap: 'wrap',
         backgroundColor: 'white',
+        paddingBottom: 40,
+    },
+    cardContainer: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingRight: 8,
+    },
+    cardContainer2: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        paddingRight: 8,
     },
     searchBar: {
         backgroundColor: 'none',
         borderTopWidth: 0,
         borderBottomWidth: 0,
         width: '100%',
+    },
+    sectionText: {
+        fontWeight: 'bold',
+        fontSize: 18,
+        paddingLeft: 8,
+    },
+    addGroupButton: {
+        aspectRatio: '1 / 1',
+        height: 130,
+        borderWidth: 2,
+        borderColor: 'black',
+        borderRadius: 30,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
     },
 });
 
